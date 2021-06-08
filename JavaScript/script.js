e60525a47d1624f7b2af725efcf75f46af45a48a
Gar Adamyan <ara50@gmail.com>
2021-06-08 22:12:19 +0400

    yaxq

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+// for(let i = 2; i <= 22; i += 2) {
+//    console.log(i); 
+// }
+
+// for(let i = 1; i <= 4; i += 1) {
+//     console.log(i);
+//     if(i = 1 * 2 * 3 * 4);
+//     console.log(i);
+// }
+
+// for(let i = 2; i <= 8; i++) {
+//     console.log(i) 
+// }
+// console.log(i = 2 + 3 + 4 + 5 + 6 + 7 + 8);
